6b50a24cf8a93759fae83e56c180a6687b35905c
Pari13123 <parsazeynali8@gmail.com>
2024-12-23 01:29:59 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('startBtn').addEventListener('click', function() {
+    window.location.href = 'https://t.me/Astrocoinn_bot'; 
+  
+  });
+  
+  document.getElementById('earnBtn').addEventListener('click', function() {
+    alert('You earned 5 points!');
+    //  
+  });
